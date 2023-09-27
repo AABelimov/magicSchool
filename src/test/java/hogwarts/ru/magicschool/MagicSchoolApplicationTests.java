54919267121b4fabff0eceae59b62b7752ab08e8
AABelimov <belimov98@gmail.com>
2023-09-27 20:29:47 +0300

    Initial commit

---
+++ src/test/java/hogwarts/ru/magicschool/MagicSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package hogwarts.ru.magicschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
